eaacd6f2ea7649cba2674ef83e1af68d9e6f9332
Ruben <rubenferraris.com>
2024-09-05 18:25:33 -0300

    laburando

--- content/js/calendary.js
+++ content/js/calendary.js
@@ -1,155 +1,62 @@
-const isLeapYear = (year) => {
-    return (
-      (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
-      (year % 100 === 0 && year % 400 === 0)
-    );
-  };
-  const getFebDays = (year) => {
-    return isLeapYear(year) ? 29 : 28;
-  };
-  let calendar = document.querySelector('.calendar');
-  const month_names = [
-      'Enero',
-      'Febrero',
-      'Marzo',
-      'Abril',
-      'Mayo',
-      'Junio',
-      'Julio',
-      'Agosto',
-      'Septiembre',
-      'Octubre',
-      'Noviembre',
-      'Diciembre',
-    ];
-  let month_picker = document.querySelector('#month-picker');
-  const dayTextFormate = document.querySelector('.day-text-formate');
-  const timeFormate = document.querySelector('.time-formate');
-  const dateFormate = document.querySelector('.date-formate');
-  
-  month_picker.onclick = () => {
-    month_list.classList.remove('hideonce');
-    month_list.classList.remove('hide');
-    month_list.classList.add('show');
-    dayTextFormate.classList.remove('showtime');
-    dayTextFormate.classList.add('hidetime');
-    timeFormate.classList.remove('showtime');
-    timeFormate.classList.add('hideTime');
-    dateFormate.classList.remove('showtime');
-    dateFormate.classList.add('hideTime');
-  };
-  
-  const generateCalendar = (month, year) => {
-    let calendar_days = document.querySelector('.calendar-days');
-    calendar_days.innerHTML = '';
-    let calendar_header_year = document.querySelector('#year');
-    let days_of_month = [
-        31,
-        getFebDays(year),
-        31,
-        30,
-        31,
-        30,
-        31,
-        31,
-        30,
-        31,
-        30,
-        31,
-      ];
-  
-    let currentDate = new Date();
-  
-    month_picker.innerHTML = month_names[month];
-  
-    calendar_header_year.innerHTML = year;
-  
-    let first_day = new Date(year, month);
-  
-  
-    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
-  
-      let day = document.createElement('div');
-  
-      if (i >= first_day.getDay()) {
-        day.innerHTML = i - first_day.getDay() + 1;
-  
-        if (i - first_day.getDay() + 1 === currentDate.getDate() &&
-          year === currentDate.getFullYear() &&
-          month === currentDate.getMonth()
-        ) {
-          day.classList.add('current-date');
+let currentMonth = document.querySelector(".current-month");
+let calendarDays = document.querySelector(".calendar-days");
+let today = new Date();
+let date = new Date();
+
+
+currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
+today.setHours(0,0,0,0);
+renderCalendar();
+
+function renderCalendar(){
+    const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
+    const totalMonthDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
+    const startWeekDay = new Date(date.getFullYear(),date.getMonth(),0).getDay();
+    
+    calendarDays.innerHTML = "";
+
+    let totalCalendarDay = 6 * 7;
+    for (let i = 0; i < totalCalendarDay; i++) {
+        let day = i-startWeekDay;
+
+        if(i <= startWeekDay){
+            // adding previous month days
+            calendarDays.innerHTML += `<div class='padding-day'>${prevLastDay-i}</div>`;
+        }else if(i <= startWeekDay+totalMonthDay){
+            // adding this month days
+            date.setDate(day);
+            date.setHours(0,0,0,0);
+            
+            let dayClass = date.getTime()===today.getTime() ? 'current-day' : 'month-day';
+            calendarDays.innerHTML += `<div class='${dayClass}'>${day}</div>`;
+        }else{
+            // adding next month days
+            calendarDays.innerHTML += `<div class='padding-day'>${day-totalMonthDay}</div>`;
         }
-      }
-      calendar_days.appendChild(day);
     }
-  };
-  
-  let month_list = calendar.querySelector('.month-list');
-  month_names.forEach((e, index) => {
-    let month = document.createElement('div');
-    month.innerHTML = `<div>${e}</div>`;
-  
-    month_list.append(month);
-    month.onclick = () => {
-      currentMonth.value = index;
-      generateCalendar(currentMonth.value, currentYear.value);
-      month_list.classList.replace('show', 'hide');
-      dayTextFormate.classList.remove('hideTime');
-      dayTextFormate.classList.add('showtime');
-      timeFormate.classList.remove('hideTime');
-      timeFormate.classList.add('showtime');
-      dateFormate.classList.remove('hideTime');
-      dateFormate.classList.add('showtime');
-    };
-  });
-  
-  (function() {
-    month_list.classList.add('hideonce');
-  })();
-  document.querySelector('#pre-year').onclick = () => {
-    --currentYear.value;
-    generateCalendar(currentMonth.value, currentYear.value);
-  };
-  document.querySelector('#next-year').onclick = () => {
-    ++currentYear.value;
-    generateCalendar(currentMonth.value, currentYear.value);
-  };
-  
-  let currentDate = new Date();
-  let currentMonth = { value: currentDate.getMonth() };
-  let currentYear = { value: currentDate.getFullYear() };
-  generateCalendar(currentMonth.value, currentYear.value);
-  
-  const todayShowTime = document.querySelector('.time-formate');
-  const todayShowDate = document.querySelector('.date-formate');
-  
-  const currshowDate = new Date();
-  const showCurrentDateOption = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long',
-  };
-  const currentDateFormate = new Intl.DateTimeFormat(
-    'en-US',
-    showCurrentDateOption
-  ).format(currshowDate);
-  todayShowDate.textContent = currentDateFormate;
-  setInterval(() => {
-    const timer = new Date();
-    const option = {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric',
-    };
-    const formateTimer = new Intl.DateTimeFormat('en-us', option).format(timer);
-    let time = `${`${timer.getHours()}`.padStart(
-        2,
-        '0'
-      )}:${`${timer.getMinutes()}`.padStart(
-        2,
-        '0'
-      )}: ${`${timer.getSeconds()}`.padStart(2, '0')}`;
-    todayShowTime.textContent = formateTimer;
-  }, 1000);
+}
+
+document.querySelectorAll(".month-btn").forEach(function (element) {
+	element.addEventListener("click", function () {
+		date = new Date(currentMonth.textContent);
+        date.setMonth(date.getMonth() + (element.classList.contains("prev") ? -1 : 1));
+		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
+		renderCalendar();
+	});
+});
+
+document.querySelectorAll(".btn").forEach(function (element) {
+	element.addEventListener("click", function () {
+        let btnClass = element.classList;
+        date = new Date(currentMonth.textContent);
+        if(btnClass.contains("today"))
+            date = new Date();
+        else if(btnClass.contains("prev-year"))
+            date = new Date(date.getFullYear()-1, 0, 1);
+        else
+            date = new Date(date.getFullYear()+1, 0, 1);
+        
+		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
+		renderCalendar();
+	});
+});
