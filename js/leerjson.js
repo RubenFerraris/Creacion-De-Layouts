dee056dd1554b409eca57c42afe130b88e2419f7
Ruben <rubenferraris@gmail.com>
2024-09-12 11:53:18 -0300

    nose

--- js/leerjson.js
+++ js/leerjson.js
@@ -8,7 +8,7 @@ fetch('js/restos.json')
     })
     .then(data => {
         data.forEach(e => {
-            body.innerHTML += 
+            body.innerHTML +=
                 `<a id="${e.name}" class="restaurante">
         <article class="oferta">
             <div class="img">
@@ -17,25 +17,10 @@ fetch('js/restos.json')
             </div>
             <div class="desc">
                 <h2>${e.name}</h2>
-                
                 <span>Hari ini pukul ${e.close}</span>
                 <div class="stats">
-                    <div class="estrellas">
-                       ${`<span class="iconos verde f13">star</span>`.repeat(e.stars)}
-                       ${`<span class="iconos f13">star</span>`.repeat(5-e.stars)}
-                       <span>(${e.cant_review})</span>
-                    </div>
-                    <div class="valor">
-                        ${`<span class="iconos verde f13">euro_symbol</span>`.repeat(e.price)}
-                        ${`<span class="iconos f13">euro_symbol</span>`.repeat(3-e.price)}
-                    </div>
                 </div>
             </div>
-            <div class="calificacion">${e.rating}</div>
         </article>
-    </a>
-    `
-
-
-        });
+    </a>`});
     })
